2125ec215a54724ee5d2ead3e1e93d5fc7ff97a8
asse_ <asro1000@stud.ek.dk>
2026-09-22 19:11:33 +0200

    Har lavet rum, med get og setters

--- src/Room.java
+++ src/Room.java
@@ -1,2 +1,54 @@
 public class Room {
+    private String navn;
+    private String beskrivelse;
+
+    //Rummene
+    private Room nord;
+    private Room øst;
+    private Room syd;
+    private Room vest;
+
+    public Room(String navn, String beskrivelse) {
+        this.navn = navn;
+        this.beskrivelse = beskrivelse;
+    }
+
+    public String getBeskrivelse() {
+        return beskrivelse;
+    }
+
+    //Nord
+    public void setNord(Room room) {
+        nord = room;
+    }
+
+    public Room getNord() {
+        return nord;
+    }
+
+    //Øst
+    public void setØst(Room room) {
+        øst = room;
+    }
+
+    public Room getØst() {
+        return øst;
+    }
+
+    //Syd
+    public void setSyd(Room room) {
+        syd = room;
+    }
+
+    public Room getSyd() {
+        return syd;
+    }
+    //Vest
+    public void setVest(Room room) {
+        vest = room;
+    }
+
+    public Room getVest() {
+        return vest;
+    }
 }
