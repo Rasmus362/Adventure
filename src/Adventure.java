da50aadea5e5dd16cd5acb172c106f551a8989b5
Rasmus362 <Milthers.rasmus@gmail.com>
2026-09-23 11:06:45 +0200

    Har indført rooms og de veje man kan tage i hvert room.
    Har indført metoderne, som switch commands kalder på for at give mulighed for bevægelsen.

--- src/Adventure.java
+++ src/Adventure.java
@@ -11,11 +11,80 @@ public class Adventure {
     Room room8 = new Room("Room 8", "Guest room nr. 1");
     Room room9 = new Room("Room 9", "Guest room nr. 2");
 
-    Adventure (){ //Sørger for at spillet altid vil starte i room1.
+    Adventure() { //Sørger for at spillet altid vil starte i room1.
         currentRoom = room1;
+
+        // Alle de forskellige veje man kan tage i spillet.
+//room 1
+        room1.setEast(room2);
+        room1.setSouth(room4);
+//room 2
+        room2.setWest(room1);
+        room2.setEast(room3);
+//room 3
+        room3.setWest(room2);
+        room3.setSouth(room6);
+//room 4
+        room4.setNorth(room1);
+        room4.setSouth(room7);
+//room 5
+        room5.setSouth(room8);
+//room 6
+        room6.setNorth(room3);
+        room6.setSouth(room9);
+//room 7
+        room7.setNorth(room4);
+        room7.setEast(room8);
+//room 8
+        room8.setNorth(room5);
+        room8.setWest(room7);
+        room8.setEast(room9);
+//room 9
+        room9.setNorth(room6);
+        room9.setWest(room8);
+
     }
 
     public Room getCurrentRoom() {
         return currentRoom;
     }
+
+    public void setCurrentRoom(Room currentRoom) {
+
+    }
+
+    //Overall for void metoderne nedenfor: Sender if statement til UserInterface, og gør det muligt at bevæge sig gennem rooms
+    public void goNorth() {
+        if (currentRoom.getNorth() != null) {
+            currentRoom = currentRoom.getNorth();
+        } else {
+            IO.println("You cannot go that way");
+        }
+
+    }
+
+    public void goEast() {
+        if (currentRoom.getEast() != null) {
+            currentRoom = currentRoom.getEast();
+        } else {
+            IO.println("You cannot go that way");
+        }
+    }
+
+    public void goSouth() {
+        if (currentRoom.getSouth() != null) {
+            currentRoom = currentRoom.getSouth();
+        } else {
+            IO.println("You cannot go that way");
+        }
+    }
+
+    public void goWest() {
+        if (currentRoom.getWest() != null) {
+            currentRoom = currentRoom.getWest();
+        } else {
+            IO.println("You cannot go that way");
+        }
+    }
+
 }
